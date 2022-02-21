03b38c70d980d48140031417b6a5b05bfc73fef0
N0S3ns3 <miknaw5@gmail.com>
2022-02-21 16:30:43 +0100

    Saving project so i can continue on my stationary computer

--- Assets/InventorySystem/CheckTarget.cs
+++ Assets/InventorySystem/CheckTarget.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TTTSC_Character_Controller_V2.Core.Scripts;
 
 public class CheckTarget : MonoBehaviour
 {
-
-    private void Update()
+    PlayerInputReceiver playerInputReceiver;
+    bool trigger = false;
+    private void OnEnable()
     {
-        if (Input.GetKeyDown(KeyCode.F))
+        playerInputReceiver = FindObjectOfType<PlayerInputReceiver>();
+
+        playerInputReceiver.inventoryInputEvent += OpenInventory;
+    }
+
+    void OpenInventory(float value)
+    {
+        if (value == 1)
         {
-            Debug.Log("Works");
+            trigger = true;
+        } else
+        {
+            trigger = false;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
         // When detected target and F pressed, destroy and save
-        if (other.gameObject.tag == "ItemTarget" && Input.GetKeyDown(KeyCode.F))
+        if (other.gameObject.tag == "ItemTarget" && trigger)
         {
             Destroy(gameObject);
         }
